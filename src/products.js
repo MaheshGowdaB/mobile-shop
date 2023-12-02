c7e089ac334087dcf62ae8ba05ccadbede08a4c7
Mahesh <maheshgowda3501@gmail.com>
2023-12-02 21:43:21 +0530

    commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 99999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "IPhone",
+    price: 122999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "IPhone",
+    price: 111699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "IPhone",
+    price: 111228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "IPhone",
+    price: 123449.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "IPhone",
+    price: 89128.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "IPhone",
+    price: 120000.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "IPhone",
+    price: 140000.0,
+    productImage: product8,
+  },
+];
